8428c6be1febf744883749f5e46c10cfc3ee3c4e
JustinRM104 <justrom104@gmail.com>
2020-10-20 10:33:25 +0200

    update

--- toets/opdracht 2/winkelwagen.js
+++ toets/opdracht 2/winkelwagen.js
@@ -1,9 +1,27 @@
 let winkelwagen = [];
 let producten = [
     ["Broek"] = [kleur = "geel", maat = "m", voorraad = 5],
+    ["Shoenen"] = [kleur = "bruin", maat = "1,42", voorraad = 10],
+    ["Shirt"] = [kleur = "groen", maat = "s", voorraad = 2],
+    ["Pet"] = [kleur = "zwart", maat = "x", voorraad = 6],
 ];
 
+function voegProduct(item) {
+    if (producten.includes(item)) {
+        winkelwagen.push(producten[item]);
+    }   
+}
 
+function getAantalWinkelmandje() {
+    console.log(winkelwagen.length);
+    return winkelwagen.length
+}
+
+function getVoorraad(item) {
+    if (producten.includes(item)) {
+        return producten[item].voorraad;
+    }   
+}
 
 function getKleurMaat(item) {
     if (producten.includes(item)) {
